21f030a4b72bacf227b5d11f3966c83557049d75
conquete.asie@gmail.com <conquete.asie@gmail.com>
2017-11-09 17:11:34 +0100

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function () {
   console.log('Connected to server');
 
-  socket.emit('createMessage', {
-    from: 'Cedric',
-    text: 'Yup, that works for me.'
-  });
 });
 
 socket.on('disconnect', function () {
